5980e20be50339cfb8949ea618b9bbc9990a321d
Christopher Mann <christopher@mann.fr>
2016-11-14 15:58:13 +0100

    Change where the email.csv file is

--- _source/import.php
+++ _source/import.php
@@ -1,6 +1,6 @@
 <?
 $email = strip_tags(trim($_POST['email']));
-$file = '../email.csv';
+$file = '../jekyll/email.csv';
 $current = file_get_contents($file);
 $current .= $email.';'."\n";
 file_put_contents($file, $current);
